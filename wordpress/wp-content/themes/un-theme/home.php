75d51158a7bc1ec991c671e556f66c878189edb3
dev <amour-youcef@hotmail.com>
2020-04-19 12:10:22 +0200

    19042020

--- wordpress/wp-content/themes/un-theme/home.php
+++ wordpress/wp-content/themes/un-theme/home.php
@@ -1,33 +1,17 @@
 <?php get_header(); ?>
 
-<!-- Partie pour afficher le contenu -->
+<ul>
+    <li id="un">poiu</li>
+    <li id="deux" >ef</li>
+    <li id="troi">zert</li>
+    <li id="quatre">cfty</li>
+</ul>
 
-<!-- les derniers article -->
-<?php $loop = new WP_Query( array( 'post_type' => 'blog', 'post_per_page' => 10, 'paged' => $paged) ); ?>
- <div class="content-home" id="home">
-
- <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-
-<?php the_terms( $post->ID, 'titre', 'Titre : ' ); ?>
-
-<div class="art-home">
-
-   <?php if (get_field('image_de_presentation') != "") {
-        ?>
-         <img class="img-home" src="<?php the_field('image_de_presentation');?>" alt="Image de présentation de l'article">
-        <?php
-     }
-    ?>
-    <div class="zone-text">
-
-        <?php 
-        the_title( '<h2 class="title-page"><a href="' . get_permalink() . '" title="' . the_title_attribute( 'echo=0' ) . '" rel="bookmark">', '</a></h2>' ); ?>
-        <p class = prez-page><?php the_field('presentation'); ?> </p>
-        <p class="postmetadata-page">   <?php the_time('j F Y') ?> par <?php the_author() ?></p>
-    </div>
-</div>
-
- <?php endwhile ; ?>
+<div>
+     <div id="pageun" class="page un">pppppppppppppppppp</div>
+     <div id="pagedeux" class="page deux hide">iiiiiiiiiiiiiiii</div>
+     <div id="pagetroi" class="page troi hide">eeeeeeeeeeeeeeeeeeee</div>
+     <div id="pagequatre" class="page quatre hide">bbbbbbbbbbbbbbbbbbbbbbbb</div>
 </div>
 
 </body> 
