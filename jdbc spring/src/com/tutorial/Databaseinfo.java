2632dde4611517ce2883a8f55da49171f2c2f376
dev <amour-youcef@hotmail.com>
2020-04-25 13:18:32 +0200

    25042020spring

---
+++ jdbc spring/src/com/tutorial/Databaseinfo.java
@@ -0,0 +1,45 @@
+package com.tutorial;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Databaseinfo {
+
+//	private JdbcTemplate template ;
+	 
+ 
+//	public void setTemplate(JdbcTemplate template) {
+//		this.template = template;
+//	}
+
+//	public int insertPerson(Person p) {
+//		String insertData = "INSERT INTO person ('fname','lname','age','address','phone')VALUES ("
+//				                                        +"'"+ p.getFname() +"'" + "," +
+//				                                         p.getLname()  +"'" + "," +
+//				                                         p.getAge()    +"'" + "," +
+//				                                         p.getAddress() +"'"+ "," +
+//				                                         p.getPhone()  +"'" + ")";
+//		
+//		return template.update(insertData);
+//	}
+	
+//	public int updatePerson(Person p) {
+//		String updateData = "UPDATE person SET ('fname','lname','age','address','phone')VALUES ("+
+//				                                  "'fname'=" + p.getFname()
+//				                                  +",'lname'=" + p.getLname()   
+//				                                  +",'age'=" + p.getAge()     
+//				                                  +",'address'=" + p.getAddress() 
+//				                                  +",'phone'=" + p.getPhone()   + "WHERE id=" +p.getId()+ ")";
+//		
+//		return template.update(updateData);
+//	}
+	
+//	public int deletePerson(Person p) {
+//		
+//		String deleteData = "DELETE FROM 'person' WHERE id="+p.getId()+  ")";
+//		
+//		return template.update(deleteData);
+//	}
+//	
+	
+}
--- jdbc spring/src/com/tutorial/Main.java
+++ jdbc spring/src/com/tutorial/Main.java
@@ -1,12 +1,22 @@
 package com.tutorial;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("test");
-
+        ApplicationContext conx = new ClassPathXmlApplicationContext("application.xml");
+		
+//        Databaseinfo di = (Databaseinfo)conx.getBean("dbi");
+	    
+//		int ins = di.insertPerson(new Person("Youcef","Essa",32,"Iraq",330608602));
+        
+//		System.out.println(ins);
+        
+        System.out.println("ok");
 	}
 
 }
