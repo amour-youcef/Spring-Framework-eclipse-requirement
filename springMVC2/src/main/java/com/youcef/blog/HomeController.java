824a03dd3f34ddb439ce849d9cbc26fdb70fa675
dev <amour-youcef@hotmail.com>
2020-04-30 00:01:12 +0200

    30042020

--- springMVC2/src/main/java/com/youcef/blog/HomeController.java
+++ springMVC2/src/main/java/com/youcef/blog/HomeController.java
@@ -1,25 +1,19 @@
 package com.youcef.blog;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.logging.Logger;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
